e0706f120ea3eab93f8a41dda9a01ff80db42fe4
Monique <monique.blom98@gmail.com>
2019-11-01 11:04:03 +0100

    Final

--- button.js
+++ button.js
@@ -1,20 +1,25 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
 var i = 0;
-function move() {
-  if (i === 0) {
-    i = 1;
-    var elem = document.getElementById("myBar");
-    var width = 10;
-    var id = setInterval (frame, 10);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-        i = 0;
-      } else {
-        width++;
-        elem.style.width = width + "%";
-        elem.innerHTML = width + "%";
-      }
+
+function move() {// Een functie wordt aangemaakt en later uitgevoerd
+    if (i === 0) {
+        i = 1;
+        var elem = document.getElementById("myBar"); // Deze code wordt uit de html gehaald met dezelfde ID
+        var width = 0;
+        var id = setInterval(frame, 10); // setInterval roept elke 10 milliseconden frame opnieuw aan. De methode setInterval blijft de functie aanroepen totdat clearInterval wordt aangeroepen of het venster wordt gesloten
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id); // De methode clearInterval wist een timerset met de methode setInterval. Is de width 100 of groter wordt setInterval gewist
+                i = 0;
+            }
+            else {
+                width++;
+                elem.style.width = width + "%";
+                elem.innerHTML = width + "%";
+            }
+        }
     }
-  }
 }
 
+// bron: W3Schools
