47ca92e8e3fb783f58ea6e74dfaa9e53122436cb
Monique <monique.blom98@gmail.com>
2019-11-01 13:52:03 +0100

    update

--- button.js
+++ button.js
@@ -1,25 +1,22 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 
-var i = 0;
-
-function move() {// Een functie wordt aangemaakt en later uitgevoerd
-    if (i === 0) {
-        i = 1;
-        var elem = document.getElementById("myBar"); // Deze code wordt uit de html gehaald met dezelfde ID
-        var width = 0;
-        var id = setInterval(frame, 10); // setInterval roept elke 10 milliseconden frame opnieuw aan. De methode setInterval blijft de functie aanroepen totdat clearInterval wordt aangeroepen of het venster wordt gesloten
-        function frame() {
-            if (width >= 100) {
-                clearInterval(id); // De methode clearInterval wist een timerset met de methode setInterval. Is de width 100 of groter wordt setInterval gewist
-                i = 0;
-            }
-            else {
-                width++;
-                elem.style.width = width + "%";
-                elem.innerHTML = width + "%";
-            }
+function update() {
+    // Een functie wordt aangemaakt en later uitgevoerd
+    var element = document.getElementById("myprogressBar");
+    //Deze code wordt uit de html gehaald met dezelfde ID
+    var width = 1;
+    var identity = setInterval(scene, 10);
+    // setInterval roept elke 10 milliseconden frame opnieuw aan. De methode setInterval blijft de functie aanroepen totdat clearInterval wordt aangeroepen of het venster wordt gesloten
+    function scene() {
+        if (width >= 100) {
+            clearInterval(identity);
+            // De methode clearInterval wist een timerset met de methode setInterval. Is de width 100 of groter wordt setInterval gewist
+        }
+        else {
+            width++;
+            element.style.width = width + '%';
+            element.innerHTML = width * 1 + '%';
         }
     }
 }
-
 // bron: W3Schools
