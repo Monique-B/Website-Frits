478fe4a11f0bf08bda8cc05e941c1e06fcc8021e
Monique <monique.blom98@gmail.com>
2019-11-13 22:30:54 +0100

    java update

--- nieuw.js
+++ nieuw.js
@@ -1,8 +1,7 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 
-const header = document.querySelector('body > header');
-const li = document.querySelector('header > li');
-
-li.addEventListener('click', function() {
-  header.classList.toggle('show');
+const button = document.querySelector("nav button");
+const aside = document.querySelector("body > aside");
+button.addEventListener("click", function(){
+    aside.classList.toggle('is-shown');
 });
