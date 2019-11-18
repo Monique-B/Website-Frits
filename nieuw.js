70379680d930b58b3e1c6ae06a4c436072fed40d
Monique <monique.blom98@gmail.com>
2019-11-18 16:58:58 +0100

    button java update

--- nieuw.js
+++ nieuw.js
@@ -2,6 +2,7 @@
 
 const button = document.querySelector("nav button");
 const aside = document.querySelector("body > aside");
+
 button.addEventListener("click", function(){
     aside.classList.toggle('is-shown');
 });
